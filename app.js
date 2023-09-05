bf796150187761aebc99c4e7e3fb05c75445c1c1
evyatarevi <evyatarevi@gmail.com>
2023-09-05 18:13:09 +0300

    05/09/2023

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const session = require("express-session"); //Third party package
+const mongodbStore = require('connect-mongodb-session');  //Third-party packages that will manage that storage for us so that we'll manage the database or a file access for us.
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
 
-const express = require('express');
-
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const MongoDBStore = mongodbStore(session); //'MongoDBStore' - is now actually a class, a constructor function we can execute to create a new object. 'mongodbStore(session)' - that internally these two things (session & mongo) can connect.
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://127.0.0.1:27017",  //path to database
+  databaseName: 'auto-demo',  // define the database in which (in it) this collection for these sessions should be created. We choose the our db.
+  collection: 'session'  //we define the collection in which our session entries will be stored.
+})
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(   // session created for every request, even unauthenticated request.
+  //Initialize and set up our session package:
+  session({
+    //accept object in parameter to configuration how to work.
+    // key names are not up to you.
+    secret: "super-secret", //'super-secret' - the string up to you. in reality we use in long and more secure string.
+    resave: false, //This will simply influence that a session is only updated in the database if the data in it is really changed. 
+    // If that would be set to true, a new session would be stored in the database for every incoming request even if nothing about the session data changed.
+    saveUninitialized: false,  //so that a session is really only stored in the database or wherever we wanna save it. Because saveUninitialized was set to false, so an empty session with no data inside won't be stored to the database, and hence no session cookie will be generated.
+    store: sessionStore  //The store setting controls where the session data actually should be stored. We use in third party package (mongodbStore) to configure to mongodb database. This  third party package manage the database for us.
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
--- routes/demo.js
+++ routes/demo.js
@@ -1,42 +1,46 @@
-const express = require('express');
-const bcryptjs = require('bcryptjs');
+const express = require("express");
+const bcryptjs = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
-
 
 //POST signup
-router.post('/signup', async function (req, res) {
+router.post("/signup", async function (req, res) {
   const userData = req.body;
   const email = userData.email;
-  const confirmEmail = userData['confirm-email']; //because dash (-) not allowed as a property name with dot notation, you have to use this alternative notation.
+  const confirmEmail = userData["confirm-email"]; //because dash (-) not allowed as a property name with dot notation, you have to use this alternative notation.
   const password = userData.password;
 
-  if(!email || !confirmEmail || !password || !email.include('@') || password.trim() < 6 || email !== confirmEmail){
-    console('Please check your details again, something went wrong')
-    return res.redirect('/signup')
+  if (
+    !email ||
+    !confirmEmail ||
+    !password ||
+    !email.include("@") ||
+    password.trim() < 6 ||
+    email !== confirmEmail
+  ) {
+    console("Please check your details again, something went wrong");
+    return res.redirect("/signup");
   }
 
-  const userExist = db.getDb().collection('users').findOne({email: email});
+  const userExist = db.getDb().collection("users").findOne({ email: email });
 
-  if(userExist){
-    console.log('The user exist, try signup with another');
-    return res.redirect('/signup');
+  if (userExist) {
+    console.log("The user exist, try signup with another");
+    return res.redirect("/signup");
   }
 
   //We need to hash the password before save it, for case that our database is hacked. We need that the hash password can't decoded back but can verify the password.
@@ -44,47 +48,78 @@ router.post('/signup', async function (req, res) {
 
   const user = {
     email: email,
-    password: hashedPassword
-  }
+    password: hashedPassword,
+  };
 
-  await db.getDb().collection('users').insertOne(user);
-  res.redirect('/login');
+  await db.getDb().collection("users").insertOne(user);
+  res.redirect("/login");
 });
 
-
 //POST login - we validation the user in the server side
-router.post('/login', async function (req, res) {
+router.post("/login", async function (req, res) {
   const userInput = req.body;
   const emailInput = userInput.email;
   const passwordInput = userInput.password;
 
-  const user = await db.getDb().collection('users').findOne({email: emailInput});  //if doesn't exist it will return 'null'.
-  if(!user){
+  const user = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: emailInput }); //if doesn't exist it will return 'null'.
+  if (!user) {
     console.log("User doesn't exist");
-    return res.status(401).render('401');
+    return res.status(401).render("401");
   }
 
   const equalPasswords = await bcryptjs.compare(passwordInput, user.password); //return true or false.
 
-  if(!equalPasswords){
+  if (!equalPasswords) {
     console.log("Wrong password");
-    return res.status(401).render('401');
+    return res.status(401).render("401");
   }
 
   // if succeed:
-  res.status(200).render('welcome');
+  //store data in the session (to see that we have a logged in user):
+  req.session.user = { id: user._id, email: user.email };
+  req.session.isAuthenticated = true; //Not necessarily.
+  /*
+  The 'session' property is provided by the express-session package which manages the session for us.
+  Every request has a session. '.user' - is new property, we could add that.
+  The value of 'user' up to you, we choose object.
+  The object saved in 'session' collection that configure up.
+
+  When response is sent, express-session package will automatically save the session to database.
+  But if you redirect to route that will soon be protected, there is a danger of this redirection being finished before
+  the session data in the database was updated, because saving to the database, as you'll learned, is an asynchronous task
+  that can take a couple of seconds or milliseconds.
+  for this exact use case here, we wanna manually call session.save - a built-in method on this session object which will 
+  force that data to be saved to the database.
+  It takes a callback function which will only be executed by this session package after saving finished.
+  */
+  req.session.save(() => {
+    res.redirect("/admin");
   });
-
-
-
-router.get('/admin', function (req, res) {
-  res.render('admin');
 });
 
-router.post('/logout', function (req, res) {});
+router.get("/admin", function (req, res) {
+  if (!req.session.isAuthenticated) {
+    //if falsy. Another option: if(!req.session.user)
+    return res.status(401).render("401");
+  }
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {
+  /* 
+  We could also delete the entire session object from the database and therefore clear to cookie, but you should think 
+  twice about because sessions can also be used for storing other data. for example shopping cart, which you might want to
+  store even for unauthenticated users.
+  In our case the session still save in the database, but the values change (also in the database): user = null, isAuthenticated = false.
+*/
+  req.session.user = null;  //falsy in if login
+  req.session.isAuthenticated = false;
+  res.redirect('/');  //we didn't need to use in req.session.save because we use that when we need to access to page that need the session. here we froward to home page that doesn't need authentication.
+});
 
 module.exports = router;
-
-
 
 //cookie - form of automatically-managed data storage in the browser
